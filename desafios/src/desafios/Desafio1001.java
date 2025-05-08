54eb977e3bb85347f08e628239f53b9be8db4d82
vanessa <vanessasantana12@outlook.com>
2025-05-08 17:06:49 -0300

    feat: resolvido o desafio 1001 - Extremamente Básico

---
+++ desafios/src/desafios/Desafio1001.java
@@ -0,0 +1,20 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class Desafio1001 {
+
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		
+		int A = input.nextInt();
+		int B = input.nextInt();
+		int x = A + B;
+		
+		System.out.println("X = " + x);
+		
+		input.close();
+
+	}
+
+}
