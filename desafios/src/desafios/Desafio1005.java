ba6ffe67710336606da05b816703df3f3d6d2203
vanessa <vanessasantana12@outlook.com>
2025-05-09 23:40:17 -0300

    Add: resolvido o desafio 1005 - Média 1

---
+++ desafios/src/desafios/Desafio1005.java
@@ -0,0 +1,26 @@
+package desafios;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Desafio1005 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		
+		double A = input.nextDouble();
+		double B = input.nextDouble();
+		
+		double media = (A * 3.5 + B * 7.5) / 11.0;
+		
+		
+		System.out.printf("MEDIA =  %.5f", media);
+		
+		input.close();
+				
+
+	}
+
+}
